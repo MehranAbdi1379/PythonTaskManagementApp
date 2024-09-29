dcac9e69f470d415548270f2a8e8d8fbf6705553
Mehran Abdi <mehranabdi13793@gmail.com>
2024-09-29 11:35:14 +0330

    Add error handeling to services

--- Services/CategoryService.py
+++ Services/CategoryService.py
@@ -2,58 +2,38 @@ from Models.Category import Category
 
 
 class CategoryService:
-    def __init__(self):
-        category1 = Category("Work")
-        category1.id = 1001
-        category2 = Category("Personal")
-        category2.id = 1002
-        category3 = Category("Shopping")
-        category3.id = 1003
-        category4 = Category("Fitness")
-        category4.id = 1004
-        category5 = Category("Hobby")
-        category5.id = 1005
-        default_category = Category("Default")
-        default_category.id = 0
-        self.categories = [
-            default_category,
-            category1,
-            category2,
-            category3,
-            category4,
-            category5,
-        ]
+    def __init__(self, category_repository):
         self.selected_category_id = 0
+        self.category_repository = category_repository
 
     def select_category(self, category_id):
-        for category in self.categories:
-            if category.id == category_id:
-                self.selected_category_id = category_id
-                print(f"Category '{category.name}' is selected")
-                return
-        print(f"No category found with Id {category_id}")
+        try:
+            category = self.category_repository.get_by_id(category_id)
+            self.selected_category_id = category_id
+            print(f"Category '{category.name}' is selected")
+        except Exception as a:
+            print("Error: " + a)
 
     def show_all_categories(self):
-        for category in self.categories:
+        categories = self.category_repository.get_all()
+        for category in categories:
             print(category)
         print()
 
     def add_category(self, category_name):
-        if category_name == "":
-            print("Please enter a valid category name!!!")
-            return
-        else:
+        try:
             category = Category(category_name)
-            self.categories.append(category)
+            self.category_repository.create(category)
             self.selected_category_id = category.id
             print(
                 f"Category '{category_name}' added successfully with ID {category.id}"
             )
+        except Exception as e:
+            print("Error: " + e)
 
     def remove_category(self, category_id):
-        for category in self.categories:
-            if category.id == category_id:
-                self.categories.remove(category)
-                print(f"Task with ID {category_id} removed successfully.")
-                return
-        print("There is no category with this ID")
+        try:
+            self.category_repository.delete(category_id)
+            print(f"Task with ID {category_id} removed successfully.")
+        except Exception as a:
+            print("Error: " + a)
--- Services/TaskService.py
+++ Services/TaskService.py
@@ -2,56 +2,32 @@ from Models.Task import Task
 
 
 class TaskService:
-    def __init__(self, category_service):
-        self.tasks = []
-        self.tasks.append(Task("Complete project report", 1))
-        self.tasks.append(Task("Buy groceries", 3))
-        self.tasks.append(Task("Morning workout", 4))
-        self.tasks.append(Task("Read a book", 5))
-        self.tasks.append(Task("Plan weekend trip", 2))
-        self.tasks.append(Task("Team meeting", 1))
-        self.tasks.append(Task("Pay bills", 3))
-        self.tasks.append(Task("Evening run", 4))
-        self.tasks.append(Task("Paint a picture", 5))
-        self.tasks.append(Task("Client call", 1))
-        self.tasks.append(Task("Clean the house", 2))
-        self.tasks.append(Task("Order supplies", 3))
-        self.tasks.append(Task("Yoga session", 4))
-        self.tasks.append(Task("Learn guitar", 5))
-        self.tasks.append(Task("Prepare presentation", 1))
-        self.tasks.append(Task("Visit the dentist", 2))
-        self.tasks.append(Task("Online shopping", 3))
-        self.tasks.append(Task("Swim practice", 4))
-        self.tasks.append(Task("Write a blog post", 5))
-        self.tasks.append(Task("Schedule appointments", 1))
-        self.category_service = category_service
+    def __init__(self, category_repository, task_repository):
+        self.category_repository = category_repository
+        self.task_repository = task_repository
 
     def show_all_tasks(self):
-        if not self.tasks:
+        if not self.task_repository.get_all():
             print("No tasks available.")
         else:
-            for task in self.tasks:
-                print(task.make_printable_task(self.category_service))
+            for task in self.task_repository.get_all():
+                print(task.make_printable_task(self.category_repository))
         print()
 
     def add_task(self, task_name, category_id):
-        if task_name == "":
-            print("The task name is not valid!!!")
-        else:
+        try:
             task = Task(task_name, category_id)
-            self.tasks.append(task)
-            category_name = ""
-            for category in self.category_service.categories:
-                if category.id == self.category_service.selected_category_id:
-                    category_name = category.name
+            self.task_repository.create(task)
+            category_name = self.category_repository.get_by_id(category_id).name
             print(
                 f"Task '{task_name}' added successfully with ID {task.id}, to the category '{category_name}'"
             )
+        except Exception as a:
+            print(a)
 
     def remove_task(self, task_id):
-        for task in self.tasks:
-            if task.id == task_id:
-                self.tasks.remove(task)
-                print(f"Task with ID {task_id} removed successfully.")
-                return
-        print(f"No task found with Id {task_id}")
+        try:
+            self.task_repository.delete(task_id)
+            print(f"Task with ID {task_id} removed successfully.")
+        except Exception as a:
+            print("Error: " + a)
