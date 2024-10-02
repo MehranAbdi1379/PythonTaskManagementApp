a448a44e4281fe58e6d5eb7702f61e6eafc39d8c
Mehran Abdi <mehranabdi13793@gmail.com>
2024-10-02 10:19:55 +0330

    Add is done to task

--- Models/Task.py
+++ Models/Task.py
@@ -6,7 +6,13 @@ class Task:
         self.name: str = name
         self.id: int = randint(1000, 9999)
         self.category_id: int = category_id
+        self.is_done = False
 
     def make_printable_task(self, category_repository):
         category_name = category_repository.get_by_id(self.category_id).name
-        return f"Task(ID: {self.id}, Name: '{self.name}', Category: {category_name})"
+        done = ""
+        if self.is_done:
+            done = "Yes"
+        else:
+            done = "No"
+        return f"Task(ID: {self.id}, Name: '{self.name}', Category: {category_name}, Done: {done})"
--- TaskApplication.py
+++ TaskApplication.py
@@ -15,19 +15,14 @@ category_service = CategoryService(category_repository, task_service)
 console_service = ConsoleService()
 
 continue_application = True
-option_counter = 5
-
 
 while continue_application:
-    if option_counter % 5 == 0:
-        console_service.print_options()
+    console_service.print_options()
 
     selected_option = input("Please enter the number of your choice: ")
 
-    continue_application = console_service.run_selected_option(
+    continue_application = console_service.run_selected_category_of_options(
         task_service, category_service, selected_option
     )
-
-    option_counter += 1
 
 print("Hope you enjoyed using the application")
