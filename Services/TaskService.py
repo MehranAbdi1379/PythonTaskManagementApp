5bb03ce656260e5d045b13afe1e09adfd3a29365
Mehran Abdi <mehranabdi13793@gmail.com>
2024-10-03 08:28:11 +0330

    Add done and undone task services

--- Services/TaskService.py
+++ Services/TaskService.py
@@ -1,16 +1,36 @@
+from __future__ import annotations
+from typing import List
 from Models.Task import Task
+from Repositories.CategoryRepository import CategoryRepository
+from Repositories.TaskRepository import TaskRepository
 
 
 class TaskService:
     def __init__(self, category_repository, task_repository):
-        self.category_repository = category_repository
-        self.task_repository = task_repository
+        self.category_repository: CategoryRepository = category_repository
+        self.task_repository: TaskRepository = task_repository
 
     def show_all_tasks(self):
         if not self.task_repository.get_all():
             print("No tasks available.")
         else:
             for task in self.task_repository.get_all():
+                print(task.make_printable_task(self.category_repository))
+        print()
+
+    def show_all_done_tasks(self):
+        if not self.task_repository.get_all_done_tasks():
+            print("No done tasks available.")
+        else:
+            for task in self.task_repository.get_all_done_tasks():
+                print(task.make_printable_task(self.category_repository))
+        print()
+
+    def show_all_undone_tasks(self):
+        if not self.task_repository.get_all_undone_tasks():
+            print("No undone tasks available.")
+        else:
+            for task in self.task_repository.get_all_undone_tasks():
                 print(task.make_printable_task(self.category_repository))
         print()
 
@@ -21,7 +41,47 @@ class TaskService:
         except Exception as e:
             print(e)
 
-    def get_tasks_by_category_id(self, category_id):
+    def remove_all_done_tasks(self):
+        try:
+            for task in self.task_repository.get_all():
+                if task.is_done == True:
+                    self.task_repository.delete(task.id)
+        except Exception as e:
+            print(e)
+
+    def remove_all_undone_tasks(self):
+        try:
+            for task in self.task_repository.get_all():
+                if task.is_done == False:
+                    self.task_repository.delete(task.id)
+        except Exception as e:
+            print(e)
+
+    def get_task_by_id(self, task_id: int) -> Task:
+        try:
+            task = self.task_repository.get_by_id(task_id)
+            return task
+        except Exception as e:
+            print(e)
+
+    def make_task_done(self, task_id: int):
+        try:
+            task = self.task_repository.get_by_id(task_id)
+            task.is_done = True
+            self.task_repository.update(task.id, task)
+            task = self.get_task_by_id(task_id)
+            print(f"Task '{task.name}' with the Id of '{task.id}' is done")
+        except Exception as e:
+            print(e)
+
+    def show_tasks_by_category_id(self, category_id: int):
+        try:
+            for task in self.get_tasks_by_category_id(category_id):
+                print(task.make_printable_task(self.category_repository))
+        except Exception as e:
+            print(e)
+
+    def get_tasks_by_category_id(self, category_id: int) -> List[Task]:
         return self.task_repository.get_by_category_id(category_id)
 
     def add_task(self, task_name, category_id):
@@ -35,10 +95,10 @@ class TaskService:
         except Exception as a:
             print(a)
 
-    def get_all_tasks(self):
+    def get_all_tasks(self) -> List[Task]:
         return self.task_repository.get_all()
 
-    def remove_task(self, task_id):
+    def remove_task(self, task_id: int):
         try:
             self.task_repository.delete(task_id)
             print(f"Task with ID {task_id} removed successfully.")
